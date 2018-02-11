dc9a0e59c827065d8c5d9e632fc46f7c26e991b2
AlejandraCP <alejandracp710@gmail.com>
2018-02-11 10:53:41 -0500

    Adding first part

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $(".button-collapse").sideNav();
+});
+
+const fixedNav = $(".navbar-fixed");
+
+let lastScroll = 0;
+$(window).scroll(e => {
+  const currScroll = $(window).scrollTop();
+  if (currScroll > lastScroll && currScroll > 200) {
+    fixedNav.css("top", "-100px");
+    fixedNav.css("transition", "top 0.3s");
+  } else {
+    fixedNav.css("top", "0");
+  }
+  lastScroll = currScroll;
+});
+
+let navBarColor = "#292929";
+$(".nav-wrapper").css("background-color", navBarColor);
